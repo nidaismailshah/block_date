d509a46106c4631e5b199f115e56c47a88f25636
Ramesh Babu G <ramesh.ganteti@popcornapps.com>
2015-02-04 19:46:29 +0530

    Fixing issues with date default value

--- js/block_by_date.js
+++ js/block_by_date.js
@@ -1,30 +1,34 @@
+/**
+ * @file
+ * Settings restricted values on date fieldset.
+ */
 (function ($) {
   Drupal.behaviors.blockByDateSettings = {
     attach: function (context) {
-     // alert('Test');
-       $('fieldset#edit-dates-between', context).drupalSetSummary(function (context) {
-      var vals = [];
-      
-	  $('input[type="checkbox"]:checked', context).each(function () {
-        //vals.push($.trim($(this).next('label').text()));
-		vals.push(Drupal.t('Restricted by certain from and to dates.'));
+      $('fieldset#edit-dates-between', context).drupalSetSummary(function (context) {
+        var vals = [];
+        
+        var todate_checkbox = $('input[type="checkbox"]:checked', context).val();
+        if ($('#edit-from-date-datepicker-popup-0', context).val() != undefined) {
+          var from_date = $('#edit-from-date-datepicker-popup-0', context).val();
+        }
+        else {
+          var from_date = $('#edit-from-date-year', context).val();
+        }
+        
+        if(from_date != '' || todate_checkbox != undefined) {
+          if(from_date != '' && todate_checkbox == undefined) {
+            vals.push(Drupal.t('Restricted by From Date.'));
+          }
+          else if((from_date != '' && todate_checkbox != undefined) || (from_date == '' && todate_checkbox != undefined)) {
+            vals.push(Drupal.t('Restricted by certain from and to dates'));
+          }
+        }
+        else {
+          vals.push(Drupal.t('Not restricted'));
+        }
+        return vals;
       });
-//alert($('#edit-from-date-datepicker-popup-0', context).val());
-      if (!vals.length) {
-		 // alert($('text[id="edit-from-date-datepicker-popup-0"]', context).val());
-		 if ($("#edit-from-date-datepicker-popup-0" ).hasClass('hasDatepicker')) {
-		if (!$('#edit-from-date-datepicker-popup-0', context).val()) {
-		  vals.push(Drupal.t('Not restricted'));
-		}
-		else {
-		  vals.push(Drupal.t('Restricted by From Date.'));          
-		}
-		 }
-      }
-      return vals;
-    });
-       
-	}
-	
+	  }
   }
 })(jQuery);

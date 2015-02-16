dad19aca38fe20c39fb3a50aa5f4faad45cf204a
Ramesh Babu G <ramesh.ganteti@popcornapps.com>
2015-02-16 14:56:20 +0530

    Ran through code revoew tools and fixed errors

--- block_by_date.js
+++ block_by_date.js
@@ -2,18 +2,17 @@
  * @file
  * Settings restricted values on date fieldset.
  */
+
 (function ($) {
   Drupal.behaviors.blockByDateSettings = {
     attach: function (context) {
       $('fieldset#edit-dates-between', context).drupalSetSummary(function (context) {
         var vals = [];
-        
-        $('#edit-from-date-timeEntry-popup-1').keydown(function(e) {  
+        $('#edit-from-date-timeEntry-popup-1').keydown(function(e) {
           if (e.keyCode == 8) {
             $('#edit-from-date-timeEntry-popup-1').val("");
           }
-        });
-        
+        });        
         var todate_checkbox = $('input[type="checkbox"]:checked', context).val();
         if ($('#edit-from-date-datepicker-popup-0', context).val() != undefined) {
           var from_date = $('#edit-from-date-datepicker-popup-0', context).val();
@@ -21,7 +20,6 @@
         else {
           var from_date = $('#edit-from-date-year', context).val();
         }
-        
         if(from_date != '' || todate_checkbox != undefined) {
           if(from_date != '' && todate_checkbox == undefined) {
             vals.push(Drupal.t('Restricted by From Date.'));
@@ -35,6 +33,6 @@
         }
         return vals;
       });
-	  }
+    }
   }
 })(jQuery);
